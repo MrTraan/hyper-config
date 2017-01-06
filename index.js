e37dc99913c72a1ccc759b09fd9da0759c19ff03
Nathan Grasset <ngrasset@student.42.fr>
2017-01-06 11:04:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const { onWindow, decorateTerms, decorateMenu } = require('./itermPaneShortcuts.js');
+const { decorateConfig } = require('./monokaiTheme.js');
+
+module.exports = {
+  onWindow,
+  decorateTerms,
+  decorateMenu,
+  decorateConfig,
+}
---
+++ itermPaneShortcuts.js
@@ -0,0 +1,106 @@
+t Mousetrap = require('mousetrap');
+
+const prevTabKey = "Ctrl+Shift+Tab";
+const nextTabKey = "Ctrl+Tab";
+const prevPaneKey = "CmdOrCtrl+[";
+const nextPaneKey = "CmdOrCtrl+]";
+
+const merge = (one, two) => Object.assign({}, one, two);
+const mousetrapify = key => key.replace('CmdOrCtrl', 'mod').toLowerCase();
+
+let focusedWindow;
+const registerWindow = win => {
+  win.on('focus', () => { focusedWindow = win; });
+  win.on('blur', () => {
+    if (focusedWindow === win) {
+      focusedWindow = undefined;
+    }
+  });
+}
+
+const selectPane = type => {
+  if (focusedWindow) {
+    focusedWindow.rpc.emit(`${type} pane req`);
+  }
+}
+
+const decorateMenu = menu => menu.map(menuItem => {
+  if (menuItem.role !== 'window') {
+    return menuItem;
+  }
+  const newMenuItem = Object.assign({}, menuItem);
+  newMenuItem.submenu = newMenuItem.submenu.map(submenuItem => {
+    const newSubmmenuItem = Object.assign({}, submenuItem);
+    switch (newSubmmenuItem.label) {
+      case 'Show Previous Tab':    newSubmmenuItem.accelerator = prevTabKey;  break;
+      case 'Show Next Tab':        newSubmmenuItem.accelerator = nextTabKey;  break;
+      case 'Select Previous Pane': newSubmmenuItem.accelerator = prevPaneKey; break;
+      case 'Select Next Pane':     newSubmmenuItem.accelerator = nextPaneKey; break;
+    }
+    return newSubmmenuItem;
+  });
+  return newMenuItem;
+});
+
+const decorateTerms = (Terms, { React, notify, Notification }) => {
+  return class extends React.Component {
+    constructor(props, context) {
+      super(props, context);
+      this.handleFocusActive = this.handleFocusActive.bind(this);
+      this.onTermsRef = this.onTermsRef.bind(this);
+    }
+    handleFocusActive() {
+      const term = this.terms.getActiveTerm();
+      if (term) {
+        term.focus();
+      }
+    }
+    attachKeyListeners() {
+      const term = this.terms.getActiveTerm();
+      if (!term) {
+        return;
+      }
+      const document = term.getTermDocument();
+      const keys = new Mousetrap(document);
+
+      keys.bind(
+          mousetrapify(prevPaneKey),
+          () => selectPane('prev')
+          );
+      keys.bind(
+          mousetrapify(nextPaneKey),
+          () => selectPane('next')
+          );
+
+      this.keys = keys;
+    }
+    onTermsRef(terms) {
+      this.terms = terms;
+    }
+    componentDidUpdate(prev) {
+      if (prev.activeSession !== this.props.activeSession) {
+        if (this.keys) {
+          this.keys.reset();
+        }
+        this.handleFocusActive();
+        this.attachKeyListeners();
+      }
+    }
+    componentWillUnmount() {
+      if (this.keys) {
+        this.keys.reset();
+      }
+    }
+    render() {
+      return React.createElement(Terms, merge(this.props, {
+        ref: this.onTermsRef
+      }));
+    }
+  }
+}
+
+module.exports = {
+  onWindow: registerWindow,
+  decorateTerms: decorateTerms,
+  decorateMenu: decorateMenu,
+}
